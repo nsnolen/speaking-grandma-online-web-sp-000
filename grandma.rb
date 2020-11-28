697fea29747c09ae897a932359e914fe706dc41c
Sara Nolen <nolensara1@gmail.com>
2020-11-28 20:12:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,11 @@
 def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
   if phrase.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.uppercase
-    true
+    return "NO, NOT SINCE 1938"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
 end
 
