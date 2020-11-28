1fbe3720a49fc7519c1cc4c32f0f6ffa07f39cc3
Sara Nolen <nolensara1@gmail.com>
2020-11-28 22:57:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,13 +5,13 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase.uppercase
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   elsif phrase.capitalize
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   else phrase.swapcase
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
 
 end
 end
