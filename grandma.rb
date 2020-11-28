7ec68a97192186fbcabfa40c96f17a16b4ac1997
Sara Nolen <nolensara1@gmail.com>
2020-11-28 20:09:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,11 +8,11 @@ def speak_to_grandma(phrase)
     true
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  
+
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
